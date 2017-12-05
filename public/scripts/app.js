65ce9932f93b319cf6f50196992246896f327e46
Jack Davis <jackdavis@10-249-205-46.wireless.oregonstate.edu>
2017-12-04 22:53:21 -0800

    Updates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,19 @@
-angular.module('fitness2Uapp', []);
+var app = angular.module('fitness2Uapp', ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "./home.html"
+    })
+    .when("/browse/:id", {
+        templateUrl : "./browse.html"
+    })
+    .when("/workout/:id", {
+        templateUrl : "./workout.html"
+    })
+    .when("/add", {
+        templateUrl : "./add.html"
+    })
+    .otherwise({
+        templateUrl : "./home.html"
+    });
+});
